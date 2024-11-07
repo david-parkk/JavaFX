881ddffce8a1f4aa8ecf753bf6c2399c816636c0
david-parkk <ji0513ji@naver.com>
2024-11-07 22:42:22 +0900

    init

---
+++ src/main/java/com/example/javafzproject/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.javafzproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
